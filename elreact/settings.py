0d0e7cd7fb2f605e6db2a6df36659afd0dcaad2d
leoscalco <leonardo.scalco@hotmail.com>
2017-11-14 17:56:34 -0200

    pronto para deploy

--- elreact/settings.py
+++ elreact/settings.py
@@ -158,6 +158,7 @@ if os.getcwd() == '/app':
     DATABASES['default'] =  dj_database_url.config()
     DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
     DATABASES['default']['NAME'] = 'elreactgis'
+    DATABASES['default']['HOST'] = 'localhost'
 
     # GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
     # Honor the 'X-Forwarded-Proto' header for request.is_secure().
