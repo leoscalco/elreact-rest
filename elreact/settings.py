a2bfac207172b7d0e07b0e20bae038658937e81a
leoscalco <leonardo.scalco@hotmail.com>
2017-11-14 18:26:23 -0200

    pronto para deploy

--- elreact/settings.py
+++ elreact/settings.py
@@ -154,6 +154,7 @@ if os.getcwd() == '/app':
     import dj_database_url
     DATABASES['default'] = dj_database_url.config()
     DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+    DATABASES['default']['NAME'] = 'elreact_geo'
     # DATABASES = {
     #     'default': dj_database_url.config(default='postgis://localhost')
     # }
