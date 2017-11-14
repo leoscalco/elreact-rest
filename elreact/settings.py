8a4138ea7d7e1e47c101eea10c99add8d0730d11
leoscalco <leonardo.scalco@hotmail.com>
2017-11-14 17:26:15 -0200

    pronto para deploy

--- elreact/settings.py
+++ elreact/settings.py
@@ -144,9 +144,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-GEOS_LIBRARY_PATH = "{}/libgeos_c.so".format(environ.get('GEOS_LIBRARY_PATH'))
-GDAL_LIBRARY_PATH = "{}/libgdal.so".format(environ.get('GDAL_LIBRARY_PATH'))
-PROJ4_LIBRARY_PATH = "{}/libproj.so".format(environ.get('PROJ4_LIBRARY_PATH'))
+GEOS_LIBRARY_PATH = environ.get('GEOS_LIBRARY_PATH')+"/libgeos_c.so"
+GDAL_LIBRARY_PATH = environ.get('GDAL_LIBRARY_PATH')+"/libgdal.so"
 
 # Heroku settings
 if os.getcwd() == '/app':
