c67672ef7fd2947194890b0dade9dce5a253621f
leoscalco <leonardo.scalco@hotmail.com>
2017-11-15 18:21:06 -0200

    deploy heroku celar 14

--- elreact/settings.py
+++ elreact/settings.py
@@ -174,4 +174,4 @@ if os.getcwd() == '/app':
     )
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'assets')
